088ec69a62dd9575c2ae6af05772ea6990819edc
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2023-02-28 21:44:50 +0330

    Add CourseRepository interface

---
+++ src/main/java/sa/aref/university/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package sa.aref.university.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sa.aref.university.entity.Professor;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Professor, Long> {
+}
