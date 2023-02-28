85f5228515bef1d63fb1eb5a0d3f69ab63413182
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2023-02-28 21:29:09 +0330

    Add Grade entity

---
+++ src/main/java/sa/aref/university/entity/Grade.java
@@ -0,0 +1,25 @@
+package sa.aref.university.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Grade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+    private String grade;
+}
