bddea20875499410c3b27988d93572e8947f7b90
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2023-02-28 20:52:13 +0330

    Add Person entity as MappedSuperClass

---
+++ src/main/java/sa/aref/university/entity/Person.java
@@ -0,0 +1,23 @@
+package sa.aref.university.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+}
